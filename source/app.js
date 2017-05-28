940b3a295d09709edda7c39c33b3db9e6d876038
Arnelle Balane <arnellebalane@gmail.com>
2017-05-28 20:55:16 +0800

    initial setup for app server

---
+++ source/app.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const consolidate = require('consolidate');
+const morgan = require('morgan');
+
+const app = express();
+
+app.engine('html', consolidate.nunjucks);
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => res.render('index.html'));
+
+module.exports = app;
