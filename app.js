3bc01a56659aee1dbe0d37bdeea980c1f8a2a52d
Arnelle Balane <arnellebalane@gmail.com>
2017-05-28 22:10:31 +0800

    add static files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.engine('html', consolidate.nunjucks);
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(morgan('dev'));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => res.render('index.html'));
 
