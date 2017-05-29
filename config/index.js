29e2b61e1614fe95bf4cd52e6ae23de28f40c670
Arnelle Balane <arnellebalane@gmail.com>
2017-05-29 15:37:33 +0800

    add name keys for nconf file configs

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ const nconf = require('nconf');
 nconf
     .argv()
     .env()
-    .file(path.join(__dirname, 'secrets.json'))
-    .file(path.join(__dirname, 'config.json'))
+    .file('secrets', path.join(__dirname, 'secrets.json'))
+    .file('config', path.join(__dirname, 'config.json'))
     .defaults({
         PORT: 3000
     });
