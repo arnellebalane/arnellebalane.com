10888d0e664d21e323f43e81e9e43d829505b2a6
Arnelle Balane <arnellebalane@gmail.com>
2017-06-21 23:23:13 +0800

    add favicons and serve using serve-favicon

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const consolidate = require('consolidate');
 const morgan = require('morgan');
+const favicon = require('serve-favicon');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.engine('html', consolidate.nunjucks);
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(morgan('dev'));
+app.use(favicon(path.join(__dirname, 'static', 'images', 'icon-16.png')));
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => res.render('index.html'));
