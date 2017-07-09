c283db388489f3f88282d6e29819e7836afa30f4
Arnelle Balane <arnellebalane@gmail.com>
2017-07-09 12:16:13 +0800

    inline critical css and dynamically load stylesheets

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -11,3 +11,19 @@ function handleRegisterSuccess(registration) {
 function handleRegisterFailure(error) {
     console.error(error);
 }
+
+
+
+// Dynamically load external stylesheets
+const stylesheets = [
+    'static/stylesheets/fonts.css',
+    'static/stylesheets/main.css'
+];
+const head = document.querySelector('head');
+
+stylesheets.forEach((stylesheet) => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = stylesheet;
+    head.appendChild(link);
+});
