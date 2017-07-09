95f49a798c70ae6a2e98819f554e606468cf8bcf
Arnelle Balane <arnellebalane@gmail.com>
2017-07-09 18:17:46 +0800

    config views directory based on NODE_ENV env variable

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const config = require('./config');
 
 const app = express();
 
-app.engine('html', consolidate.nunjucks);
-app.set('views', path.join(__dirname, 'views'));
-
+const VIEWS_PATH = config.get('NODE_ENV') === 'production'
+    ? path.join(__dirname, 'build', 'views')
+    : path.join(__dirname, 'views');
 const STATIC_PATH = config.get('NODE_ENV') === 'production'
     ? path.join(__dirname, 'build', 'static')
     : path.join(__dirname, 'static');
@@ -18,6 +18,9 @@ const SERVICE_WORKER_PATH = path.join(STATIC_PATH, 'javascripts', 'sw.js');
 const OFFLINE_GOOGLE_ANALYTICS_PATH = path.join(__dirname, 'node_modules',
     'sw-offline-google-analytics', 'build', 'importScripts',
     'sw-offline-google-analytics.prod.v0.0.25.js');
+
+app.engine('html', consolidate.nunjucks);
+app.set('views', VIEWS_PATH);
 
 app.use(morgan('dev'));
 app.use(favicon(FAVICON_PATH));
