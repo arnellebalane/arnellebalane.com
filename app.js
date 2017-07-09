e986a49d07c5f934c61ae3034e210d2aa2412f46
Arnelle Balane <arnellebalane@gmail.com>
2017-07-09 18:15:21 +0800

    update static path based on NODE_ENV env variable

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const express = require('express');
 const consolidate = require('consolidate');
 const morgan = require('morgan');
 const favicon = require('serve-favicon');
+const config = require('./config');
 
 const app = express();
 
 app.engine('html', consolidate.nunjucks);
 app.set('views', path.join(__dirname, 'views'));
 
-const FAVICON_PATH = path.join(__dirname, 'static', 'images', 'icon-16.png');
-const STATIC_PATH = path.join(__dirname, 'static');
-const SERVICE_WORKER_PATH = path.join(__dirname, 'static', 'javascripts', 'sw.js');
+const STATIC_PATH = config.get('NODE_ENV') === 'production'
+    ? path.join(__dirname, 'build', 'static')
+    : path.join(__dirname, 'static');
+const FAVICON_PATH = path.join(STATIC_PATH, 'images', 'icon-16.png');
+const SERVICE_WORKER_PATH = path.join(STATIC_PATH, 'javascripts', 'sw.js');
 const OFFLINE_GOOGLE_ANALYTICS_PATH = path.join(__dirname, 'node_modules',
     'sw-offline-google-analytics', 'build', 'importScripts',
     'sw-offline-google-analytics.prod.v0.0.25.js');
