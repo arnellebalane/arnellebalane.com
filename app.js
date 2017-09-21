5f1bc985f36b2a1d01a99b7f737e25000b050e72
Arnelle Balane <arnellebalane@gmail.com>
2017-09-21 17:31:52 +0800

    add routes for medium module

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/github', require('./lib/github/routes'));
+app.use('/medium', require('./lib/medium/routes'));
 app.use('/amp', require('./lib/amp/routes'));
 
 app.get('/', (req, res) => {
