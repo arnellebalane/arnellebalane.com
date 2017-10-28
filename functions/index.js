04e17c88312aa79aab82ae6266e8817156a35d12
Arnelle Balane <arnellebalane@gmail.com>
2017-10-29 02:37:39 +0800

    add https function ifttt webhook for github data

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+const database = admin.database();
+
+exports.githubDataWebhook = functions.https.onRequest((req, res) => {
+    if (req.method !== 'POST') {
+        return res.sendStatus(404);
+    }
+    database.ref('data/github').once('value', (snapshot) => {
+        const githubData = snapshot.val();
+        const updatedData = [req.body, ...githubData.slice(0, 2)];
+        snapshot.ref.set(updatedData)
+            .then(() => res.sendStatus(200));
+    });
+});
