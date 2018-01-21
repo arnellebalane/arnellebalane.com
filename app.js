0d383d8fd5fa275d579725f112fd839f82a15365
Arnelle Balane <arnellebalane@gmail.com>
2018-01-21 22:22:22 +0800

    set maxage in express.static for static files

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const SERVICE_WORKER_PATH = path.join(STATIC_PATH, 'javascripts', 'sw.js');
 const OFFLINE_GOOGLE_ANALYTICS_PATH = path.join(__dirname, 'node_modules',
     'sw-offline-google-analytics', 'build', 'importScripts',
     'sw-offline-google-analytics.prod.v0.0.25.js');
+const expressStaticOptions = {
+    maxage: '1h'
+};
 
 app.engine('html', consolidate.nunjucks);
 app.set('views', VIEWS_PATH);
@@ -28,10 +31,10 @@ app.use(helmet());
 app.use(morgan('dev'));
 app.use(bodyparser.json());
 app.use(favicon(FAVICON_PATH));
-app.use('/static', express.static(STATIC_PATH));
+app.use('/static', express.static(STATIC_PATH), expressStaticOptions);
 app.use('/sw.js', express.static(SERVICE_WORKER_PATH));
 app.use('/sw-offline-google-analytics.js',
-    express.static(OFFLINE_GOOGLE_ANALYTICS_PATH));
+    express.static(OFFLINE_GOOGLE_ANALYTICS_PATH), expressStaticOptions);
 app.use((req, res, next) => {
     res.locals.req = req;
     next();
