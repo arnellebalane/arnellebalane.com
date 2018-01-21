2c3c61cbd78e8a73322a8ee63a7bd3c05991aa07
Arnelle Balane <arnellebalane@gmail.com>
2018-01-21 21:39:08 +0800

    bump sw cache version

--- static/javascripts/sw.js
+++ static/javascripts/sw.js
@@ -1,7 +1,7 @@
 importScripts('sw-offline-google-analytics.js');
 goog.offlineGoogleAnalytics.initialize();
 
-const CACHE_NAME = 'arnelle-v1.12';
+const CACHE_NAME = 'arnelle-v1.13';
 const CACHE_PATHS = [
     '/',
     '/static/manifest.json',
