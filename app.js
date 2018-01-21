600b939c1587ec2f8e854f9f25c609a8f0881c73
Arnelle Balane <arnellebalane@gmail.com>
2018-01-21 21:19:44 +0800

    add module for serving upcoming talk data

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use((req, res, next) => {
 
 app.use('/github', require('./lib/github/routes'));
 app.use('/medium', require('./lib/medium/routes'));
+app.use('/upcoming-talk', require('./lib/talks/routes'));
 app.use('/amp', require('./lib/amp/routes'));
 app.use('/subscriptions', require('./lib/notifications/routes'));
 
