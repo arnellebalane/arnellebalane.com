e5279c65d4d9153c17dfc005e5b862cc100b9dfa
Arnelle Balane <arnellebalane@gmail.com>
2018-01-23 11:32:04 +0800

    bump service worker version

--- static/javascripts/sw.js
+++ static/javascripts/sw.js
@@ -1,7 +1,7 @@
 importScripts('sw-offline-google-analytics.js');
 goog.offlineGoogleAnalytics.initialize();
 
-const CACHE_NAME = 'arnelle-v1.15';
+const CACHE_NAME = 'arnelle-v1.16';
 const CACHE_PATHS = [
     '/',
     '/static/manifest.webmanifest',
