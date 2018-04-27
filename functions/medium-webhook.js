824e84d337f4e16ac5d0e54a2a85c7961817a261
Arnelle Balane <arnellebalane@gmail.com>
2018-04-28 00:43:20 +0800

    add function to handle medium webhook

---
+++ functions/medium-webhook.js
@@ -0,0 +1,24 @@
+const dataChanger = require('./data-changer');
+
+const dataPath = 'data/articles.json';
+const endpoint = `/repos/arnellebalane/arnellebalane.com/contents/${dataPath}`;
+
+exports.handler = async (event, context, callback) => {
+    if (event.httpMethod !== 'POST') {
+        return callback(null, { statusCode: 404 });
+    }
+
+    const body = JSON.parse(event.body);
+    const { sha } = await dataChanger.getData(endpoint);
+
+    const updated = [ body ];
+    const options = {
+        path: dataPath,
+        message: 'Updated articles.json data file',
+        content: JSON.stringify(updated),
+        sha
+    };
+    await dataChanger.setData(endpoint, options);
+
+    callback(null, { statusCode: 200 });
+};
