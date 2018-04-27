7b3435d1cee5b601005b724a72d45b865c28366e
Arnelle Balane <arnellebalane@gmail.com>
2018-04-27 10:09:43 +0800

    use html-webpack-plugin to generate html template in build directory

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -9,5 +10,10 @@ export default {
         publicPath: '/',
         filename: 'index.bundle.js'
     },
-    mode: NODE_ENV
+    mode: NODE_ENV,
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'source/index.html')
+        })
+    ]
 };
