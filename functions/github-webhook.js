4f0172a9e56c57fe5c384ae56507ca9678586f07
Arnelle Balane <arnellebalane@gmail.com>
2018-04-28 12:23:56 +0800

    move data-changer code into its own module

--- functions/github-webhook.js
+++ functions/github-webhook.js
@@ -1,39 +1,14 @@
-const axios = require('axios');
-
-axios.defaults.baseURL = 'https://api.github.com';
-axios.defaults.headers['Authorization'] = `token ${process.env.GITHUB_TOKEN}`;
+const dataChanger = require('./utils/data-changer');
 
 const dataPath = 'data/projects.json';
 const endpoint = `/repos/arnellebalane/arnellebalane.com/contents/${dataPath}`;
-
-function getData(endpoint) {
-    return axios.get(endpoint)
-        .then(response => response.data)
-        .then(data => ({
-            content: Buffer.from(data.content, 'base64').toString('utf8'),
-            sha: data.sha
-        }));
-}
-
-function setData(endpoint, options) {
-    return axios.put(endpoint, {
-        path: options.path,
-        message: options.message,
-        content: Buffer.from(options.content).toString('base64'),
-        committer: {
-            name: 'data[bot]',
-            email: 'data[bot]@arnelle.me'
-        },
-        sha: options.sha
-    });
-}
 
 exports.handler = (event, context, callback) => {
     if (event.httpMethod !== 'POST') {
         return callback(null, { statusCode: 404 });
     }
 
-    getData(endpoint).then(({ content, sha }) => {
+    dataChanger.getData(endpoint).then(({ content, sha }) => {
         const body = JSON.parse(event.body);
         const updated = [ body, ...content.slice(0, 2) ];
         const options = {
@@ -43,6 +18,6 @@ exports.handler = (event, context, callback) => {
             sha
         };
 
-        return setData(endpoint, options);
+        return dataChanger.setData(endpoint, options);
     }).then(() => callback(null, { statusCode: 200 }));
 };
