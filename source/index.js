12ed214a65b924e8bb714636e076f9337a273826
Arnelle Balane <arnellebalane@gmail.com>
2018-04-28 17:47:58 +0800

    register service worker in index.js

--- source/index.js
+++ source/index.js
@@ -3,3 +3,9 @@ import './index.css';
 if (process.env.NODE_ENV === 'production') {
     require('./scripts/google-analytics').initialize();
 }
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('sw.js').then(registration => {
+        console.log('Service worker registered successfully.');
+    });
+}
