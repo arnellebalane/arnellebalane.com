28bcb0ff943f00d914b8a32a6d8ae71fdf1bce7e
Arnelle Balane <arnellebalane@gmail.com>
2018-05-15 18:59:24 -0700

    remove google analytics in codebase and inject it via netlify

--- source/index.js
+++ source/index.js
@@ -1,9 +1,5 @@
 import('./index.css');
 
-if (process.env.NODE_ENV === 'production') {
-    require('./scripts/google-analytics').initialize();
-}
-
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('sw.js').then(registration => {
         console.log('Service worker registered successfully.');
