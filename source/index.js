aa86f28787317d4630cc2c3a82a0409ee75d16a6
Arnelle Balane <arnellebalane@gmail.com>
2018-05-15 19:59:57 -0700

    remove manual service worker registration

--- source/index.js
+++ source/index.js
@@ -1,7 +1,1 @@
 import('./index.css');
-
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('sw.js').then(registration => {
-        console.log('Service worker registered successfully.');
-    });
-}
