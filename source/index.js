945f62f1fb932d43a3ba03f3eb245f0db167efa4
Arnelle Balane <arnellebalane@gmail.com>
2018-06-08 19:26:05 +0800

    add button to toggle dark/light themes

--- source/index.js
+++ source/index.js
@@ -3,3 +3,9 @@ import './index.css';
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('sw.js');
 }
+
+const themeButton = document.querySelector('.toggle-theme');
+
+themeButton.addEventListener('click', () => {
+    document.querySelector('html').classList.toggle('dark');
+});
