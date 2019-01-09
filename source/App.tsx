a6e82a4561770c5020c4d3c78df93453f8ae7e59
Arnelle Balane <arnellebalane@gmail.com>
2019-01-10 01:12:43 +0800

    Use react router components in top-level component

--- source/App.tsx
+++ source/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.tsx';
+import Home from './pages/Home.tsx';
 
 export default function App(props) {
     return (
-        <Navbar />
+        <React.Fragment>
+            <Navbar />
+
+            <BrowserRouter>
+                <Route path="/" exact component={Home} />
+            </BrowserRouter>
+        </React.Fragment>
     );
 };
