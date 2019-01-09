1bdc1f4e830fb753546200cbba03782170a1cfa3
Arnelle Balane <arnellebalane@gmail.com>
2019-01-10 00:06:57 +0800

    Add styles for Navbar component

--- source/components/Navbar/Navbar.tsx
+++ source/components/Navbar/Navbar.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Avatar from '@/images/avatar.jpg';
+import style from './Navbar.css';
 
 export default function Navbar(props) {
     return (
-        <header>
-            <img src={Avatar} />
-            <h1>@arnellebalane</h1>
+        <header className={style.header}>
+            <img
+                className={style.avatar}
+                src={Avatar}
+            />
+
+            <h1 className={style.title}>
+                @arnellebalane
+            </h1>
+
+            <button className={style.menu}>
+                Toggle Menu
+            </button>
         </header>
     );
 };
