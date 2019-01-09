fdbe5cdab733367886457fafdfa6e648bb4af3ce
Arnelle Balane <arnellebalane@gmail.com>
2019-01-09 23:34:19 +0800

    Render top-level component with ReactDOM

--- source/index.tsx
+++ source/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.render(
-    <h1>@arnellebalane</h1>,
+    <App />,
     document.querySelector('#root')
 );
