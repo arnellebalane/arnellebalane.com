7611d93b2c42a6e4d2e586250021a63b4005cd7f
Arnelle Balane <arnellebalane@gmail.com>
2019-01-09 19:55:41 +0800

    Improve BASE_URL detection in config file

--- config/index.js
+++ config/index.js
@@ -1,4 +1,11 @@
+const {
+    NODE_ENV = 'development',
+    CONTEXT = 'production',
+    URL = 'https://arnellebalane.com/',
+    DEPLOY_URL = 'https://arnellebalane.com/'
+} = process.env;
+
 export default {
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    BASE_URL: process.env.URL || 'https://arnellebalane.com/'
+    NODE_ENV,
+    BASE_URL: CONTEXT === 'production' ? URL : DEPLOY_URL
 };
