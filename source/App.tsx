e6331717d9c782f5fc4b4978bdbbc3fe28e42945
Arnelle Balane <arnellebalane@gmail.com>
2019-01-09 23:34:02 +0800

    Define top-level component

---
+++ source/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './components/Navbar/Navbar.tsx';
+
+export default function App(props) {
+    return (
+        <Navbar />
+    );
+};
