87af8eb59e6dd622a5c5f81cc8c59af3781c6734
Arnelle Balane <arnellebalane@gmail.com>
2019-01-10 01:44:38 +0800

    Style App component to make Footer sticky to bottom

--- source/App.tsx
+++ source/App.tsx
@@ -3,17 +3,18 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.tsx';
 import Footer from './components/Footer/Footer.tsx';
 import Home from './pages/Home/Home.tsx';
+import style from './App.css';
 
 export default function App(props) {
     return (
         <BrowserRouter>
-            <React.Fragment>
+            <div className={style.wrapper}>
                 <Navbar />
 
                 <Route path="/" exact component={Home} />
 
                 <Footer />
-            </React.Fragment>
+            </div>
         </BrowserRouter>
     );
 };
