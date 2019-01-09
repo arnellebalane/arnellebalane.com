15471f48fe6d3a32e3bb9ac62cbeedd561495593
Arnelle Balane <arnellebalane@gmail.com>
2019-01-10 01:35:44 +0800

    Render Footer in top-level App component

--- source/App.tsx
+++ source/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar.tsx';
+import Footer from './components/Footer/Footer.tsx';
 import Home from './pages/Home/Home.tsx';
 
 export default function App(props) {
@@ -10,6 +11,8 @@ export default function App(props) {
                 <Navbar />
 
                 <Route path="/" exact component={Home} />
+
+                <Footer />
             </React.Fragment>
         </BrowserRouter>
     );
