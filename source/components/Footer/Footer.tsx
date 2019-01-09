465458d346eb2a16e2e7936dde6ebf7427e5c503
Arnelle Balane <arnellebalane@gmail.com>
2019-01-10 01:35:30 +0800

    Define and style Footer component

---
+++ source/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Footer.css';
+
+export default function Footer(props) {
+    return (
+        <footer className={style.footer}>
+            <small>Copyright &copy; 2019 Arnelle Balane</small>
+        </footer>
+    );
+};
