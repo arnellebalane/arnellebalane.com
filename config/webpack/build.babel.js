72d7c537b423f6ddae5dbb2e7e7b63bacd591e5f
Arnelle Balane <arnellebalane@gmail.com>
2019-01-09 19:33:33 +0800

    Pass options to HtmlWebpackPlugin to use custom html template

--- config/webpack/build.babel.js
+++ config/webpack/build.babel.js
@@ -1,19 +1,33 @@
 import path from 'path';
 import merge from 'webpack-merge';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import baseConfig from './base.babel';
+import baseWebpackConfig from './base.babel';
+import config from '../index';
 
-export default merge(baseConfig, {
+function resolvePath(relativePath) {
+    return path.resolve(__dirname, `../../${relativePath}`)
+}
+
+export default merge(baseWebpackConfig, {
     entry: '@/index.js',
 
     output: {
-        path: path.resolve(__dirname, '../../build'),
+        path: resolvePath('build'),
         publicPath: '/',
         filename: '[name].[hash:6].js',
         chunkFilename: '[name].[hash:6].js'
     },
 
     plugins: [
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({
+            template: resolvePath('source/index.html'),
+            templateParameters: {
+                baseUrl: config.BASE_URL
+            },
+            minify: {
+                collapseBooleanAttributes: true,
+                collapseWhitespace: true
+            }
+        })
     ]
 });
