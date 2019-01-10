5efb53e8d1a78ca2a083c22730412ddb3e0b696a
Arnelle Balane <arnellebalane@gmail.com>
2019-01-10 12:53:11 +0800

    Move shared card styles into stylesheets/cards.css

--- source/components/ArticleCard/ArticleCard.tsx
+++ source/components/ArticleCard/ArticleCard.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import style from './ArticleCard.css';
+import shared from '@/stylesheets/cards.css';
 
 export default function ArticleCard(props) {
     const article = props.article;
 
     return (
-        <article className={style.article}>
-            <Link className={style.link} to={article.url}>
-                <h1 className={style.title}>
+        <article className={shared.card}>
+            <Link className={shared.link} to={article.url}>
+                <h1 className={shared.title}>
                     {article.title}
                 </h1>
             </Link>
 
             <time
-                className={style.datePublished}
+                className={shared.subtitle}
                 dateTime={article.date_published}
             >
                 {article.date_published}
             </time>
 
-            <p className={style.summary}>
+            <p className={shared.content}>
                 {article.summary}
             </p>
         </article>
