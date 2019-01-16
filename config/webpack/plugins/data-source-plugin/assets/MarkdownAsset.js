9cf4ddce3f110147240ba4c596b586b8e3f2dcd8
Arnelle Balane <arnellebalane@gmail.com>
2019-01-16 21:15:03 +0800

    Remove .md extension for generated markdown files

--- config/webpack/plugins/data-source-plugin/assets/MarkdownAsset.js
+++ config/webpack/plugins/data-source-plugin/assets/MarkdownAsset.js
@@ -1,6 +1,13 @@
 import Asset from './Asset';
 
 export default class MarkdownAsset extends Asset {
+    constructor(...args) {
+        super(...args);
+
+        this.inputRelativePath = this.inputRelativePath.replace(/\.md$/, '');
+        this.outputKey = this.outputKey.replace(/\.md$/, '');
+    }
+
     getContent() {
         return JSON.stringify(this.content, null, '  ');
     }
