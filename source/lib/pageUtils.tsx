ee06428e4f198f4983c79246931e056a44da098d
Arnelle Balane <arnellebalane@gmail.com>
2019-01-21 19:38:30 +0800

    Define util module for extracting/constructing API pages

---
+++ source/lib/pageUtils.tsx
@@ -0,0 +1,11 @@
+const pageFormat = process.env.API_PAGE_FORMAT;
+const pageFormatRegex = new RegExp(pageFormat.replace('{page}', '(\\d+)'));
+
+export function extractPage(url) {
+    const match = url.match(pageFormatRegex);
+    return match ? parseInt(match[1], 10) : 1;
+}
+
+export function constructPage(page) {
+    return pageFormat.replace('{page}', page);
+}
