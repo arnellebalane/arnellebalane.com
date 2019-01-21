668039516b6657cdd06e46b1ee1e395edf8e6c45
Arnelle Balane <arnellebalane@gmail.com>
2019-01-21 19:48:24 +0800

    Fix slashes for API_ENDPOINT value construction

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@
 const {
     NODE_ENV = 'development',
     CONTEXT = 'development',
-    URL = 'http://localhost:8081/',
-    DEPLOY_URL = 'http://localhost:8081/',
+    URL = 'http://localhost:8081',
+    DEPLOY_URL = 'http://localhost:8081',
     API_PAGE_FORMAT = '/pages/{page}.json',
     API_PATH_EXTENSION = '.json'
 } = process.env;
@@ -12,6 +12,6 @@ export default {
     NODE_ENV,
     API_PAGE_FORMAT,
     API_PATH_EXTENSION,
-    API_ENDPOINT: (CONTEXT === 'production' ? URL : DEPLOY_URL) + 'api/',
+    API_ENDPOINT: (CONTEXT === 'production' ? URL : DEPLOY_URL) + '/api/',
     BASE_URL: (CONTEXT === 'production' ? URL : DEPLOY_URL).replace(/\/$/, '')
 };
