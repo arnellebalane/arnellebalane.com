50dec81344cd7e76ae639d93e31c377171c82ae4
Arnelle Balane <arnellebalane@gmail.com>
2019-01-21 21:48:24 +0800

    Check for a fixed known value for API_PATH_EXTENSION

--- config/index.js
+++ config/index.js
@@ -5,13 +5,21 @@ const {
     URL = 'http://localhost:8081',
     DEPLOY_URL = 'http://localhost:8081',
     API_PAGE_FORMAT = '/pages/{page}.json',
-    API_PATH_EXTENSION = '.json'
+    API_PATH_EXTENSION
 } = process.env;
 
 export default {
     NODE_ENV,
     API_PAGE_FORMAT,
-    API_PATH_EXTENSION,
+
+    /*
+     * Manually checking the env variable here, since I can't rely on empty
+     * strings for there values because they seem to be ignored. Therefore I'm
+     * just manually setting the default value when the provided value matches
+     * some known constant.
+     */
+    API_PATH_EXTENSION: API_PATH_EXTENSION === '[none]' ? '' : '.json',
+
     API_ENDPOINT: (CONTEXT === 'production' ? URL : DEPLOY_URL) + '/api/',
     BASE_URL: (CONTEXT === 'production' ? URL : DEPLOY_URL).replace(/\/$/, '')
 };
