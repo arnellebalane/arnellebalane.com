88d2ff57255e086a222637ffbca60a2983256fcf
Arnelle Balane <arnellebalane@gmail.com>
2019-01-21 22:20:52 +0800

    Create useApiData hook to abstract fetching data from api

--- source/pages/Articles/Articles.tsx
+++ source/pages/Articles/Articles.tsx
@@ -1,34 +1,15 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {Link, withRouter} from 'react-router-dom';
 import ArticleCard from './components/ArticleCard/ArticleCard.tsx';
-import fetchData from '@/lib/fetchData.tsx';
-import {extractPage, constructPage} from '@/lib/pageUtils.tsx';
+import useApiData from '@/lib/useApiData.tsx';
 import shared from '@/stylesheets/pages.css';
 
-function ArticleList({location: _location}) {
-    const [articles, setArticles] = useState([]);
-    const [pages, setPages] = useState({
-        nextPage: null,
-        previousPage: null
-    });
-
-    useEffect(() => {
-        const queries = new URLSearchParams(_location.search);
-        const page = queries.has('page') ? parseInt(queries.get('page'), 10) : null;
-        const endpoint = page ? `articles${constructPage(page)}` : 'articles';
-
-        fetchData(endpoint).then(({data: response}) => {
-            setArticles(response.data);
-            setPages({
-                nextPage: response.next_page
-                    ? `${_location.pathname}?page=${extractPage(response.next_page)}`
-                    : null,
-                previousPage: response.previous_page
-                    ? `${_location.pathname}?page=${extractPage(response.previous_page)}`
-                    : null
-            });
-        });
-    }, [_location.search]);
+function ArticleList(props) {
+    const {
+        data: articles,
+        nextPage,
+        previousPage
+    } = useApiData('articles', props.location);
 
     return (
         <div>
@@ -41,14 +22,14 @@ function ArticleList({location: _location}) {
             ))}
 
             <div className={shared.links}>
-                {pages.nextPage && (
-                    <Link className={shared.link} to={pages.nextPage}>
+                {nextPage && (
+                    <Link className={shared.link} to={nextPage}>
                         See older articles
                     </Link>
                 )}
 
-                {pages.previousPage && (
-                    <Link className={shared.link} to={pages.previousPage}>
+                {previousPage && (
+                    <Link className={shared.link} to={previousPage}>
                         See newer articles
                     </Link>
                 )}
