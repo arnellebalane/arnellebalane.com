ac36da1a552379abda2611805e1cf7ef103341e4
Arnelle Balane <arnellebalane@gmail.com>
2019-07-24 03:42:13 +0800

    Add layout partial, initial styles, and Hero section

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = config => {
+    return {
+        templateFormats: [
+            // These are recognized template formats and will be processed
+            // by Eleventy.
+            'njk', 'md', 'html',
+
+            // These are unrecognized template formats and will just be copied
+            // directly by Eleventy.
+            'css', 'jpg', 'svg'
+        ]
+    };
+};
