eb4f11acf822271d1acd5b59ac7e6a7831c84c44
Arnelle Balane <arnellebalane@gmail.com>
2019-07-27 00:45:47 +0800

    Update theme-color meta on color scheme change

--- source/static/javascripts/main.mjs
+++ source/static/javascripts/main.mjs
@@ -1,1 +1,15 @@
 import * as DarkModeToggle from './custom-elements/dark-mode-toggle.mjs';
+
+const themeColors = {
+    dark: '#141A24',
+    light: '#fff'
+};
+
+const themeColor = document.querySelector('meta[name="theme-color"]');
+const darkModeToggle = document.querySelector('dark-mode-toggle');
+const updateThemeColor = () => {
+    themeColor.content = themeColors[darkModeToggle.mode];
+};
+updateThemeColor();
+
+document.addEventListener('colorschemechange', updateThemeColor);
