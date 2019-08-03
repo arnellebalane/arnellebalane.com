2344cb570468e877467e9da032b0221634d67fbf
Arnelle Balane <arnellebalane@gmail.com>
2019-08-03 15:30:26 +0800

    Add runtime caching for HTML pages

--- source/sw.js
+++ source/sw.js
@@ -2,3 +2,14 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 
 workbox.core.skipWaiting();
 workbox.precaching.precacheAndRoute([]);
+
+// NOTE: All HTML pages should be dynamically cached, and also constantly
+// revalidated to make sure that the cache is always up-to-date.
+
+const staticUrlRegex = /\/static\//;
+const notStaticUrl = ({url}) => !staticUrlRegex.test(url);
+
+workbox.routing.registerRoute(
+    notStaticUrl,
+    new workbox.strategies.StaleWhileRevalidate()
+);
