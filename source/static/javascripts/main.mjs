733f4e7a1b07017896dcc7604d99122f2fe9bf74
Arnelle Balane <arnellebalane@gmail.com>
2019-08-03 23:32:10 +0800

    Show snackbar when page update is available

--- source/static/javascripts/main.mjs
+++ source/static/javascripts/main.mjs
@@ -40,10 +40,23 @@ if ('serviceWorker' in navigator) {
     channel.addEventListener('message', event => {
         const url = event.data.payload.updatedURL;
         if (url === location.href) {
-            const reload = confirm('Page updated. Reload?');
-            if (reload) {
-                location.reload();
-            }
+            displaySnackbar({
+                text: 'Page update available.',
+                action: 'Refresh',
+                callback: () => location.reload()
+            });
         }
     });
+}
+
+async function displaySnackbar({text, action, callback}={}) {
+    document.head.insertAdjacentHTML('beforeend',
+        '<link rel="stylesheet" href="/static/stylesheets/components/snackbars.css">'
+    );
+    document.body.insertAdjacentHTML('beforeend', `
+        <div class="Snackbar">
+            <p class="Snackbar__Text">${text}</p>
+            <button class="Snackbar__Action" onclick="(${callback})()">${action}</button>
+        </div>
+    `);
 }
