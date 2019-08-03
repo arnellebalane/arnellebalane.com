566e1a4a9f062174796747c30047dd50dd01013d
Arnelle Balane <arnellebalane@gmail.com>
2019-08-03 15:19:03 +0800

    Run skipWaiting using workbox instead of custom

--- source/sw.js
+++ source/sw.js
@@ -1,7 +1,4 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
+workbox.core.skipWaiting();
 workbox.precaching.precacheAndRoute([]);
-
-addEventListener('install', event => {
-    event.waitUntil(skipWaiting());
-});
