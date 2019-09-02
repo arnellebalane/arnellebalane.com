8be7c5b1a74edb54c8a729cfe415128c985ec0cb
Arnelle Balane <arnellebalane@gmail.com>
2019-09-02 21:38:01 +0800

    Update font subsets to include US_ASCII

--- scripts/subset-fonts.sh
+++ scripts/subset-fonts.sh
@@ -2,4 +2,4 @@
 
 glyphhanger --formats=woff2 --subset=source/static/fonts/Gilroy/Gilroy-Regular.ttf --US_ASCII
 glyphhanger --formats=woff2 --subset=source/static/fonts/Gilroy/Gilroy-Heavy.ttf --US_ASCII
-glyphhanger --formats=woff2 --subset=source/static/fonts/Gilroy/Gilroy-Bold.ttf --whitelist=@arnellebalane
+glyphhanger --formats=woff2 --subset=source/static/fonts/Gilroy/Gilroy-Bold.ttf --US_ASCII
