34e7188c5cf0ebde05975b0cd92184f5aee69261
Arnelle Balane <arnellebalane@gmail.com>
2019-09-05 22:09:48 +0800

    Use both woff and woff2, but preload woff2 fonts

--- scripts/subset-fonts.sh
+++ scripts/subset-fonts.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-glyphhanger --formats=woff --subset=source/static/fonts/Inter/Inter-900-normal.woff --US_ASCII
-glyphhanger --formats=woff --subset=source/static/fonts/Inter/Inter-900-italic.woff --US_ASCII
-glyphhanger --formats=woff --subset=source/static/fonts/Inter/Inter-700-normal.woff --US_ASCII
-glyphhanger --formats=woff --subset=source/static/fonts/Inter/Inter-700-italic.woff --US_ASCII
-glyphhanger --formats=woff --subset=source/static/fonts/Inter/Inter-400-italic.woff --US_ASCII
-glyphhanger --formats=woff --subset=source/static/fonts/Inter/Inter-400-normal.woff --US_ASCII
-glyphhanger --formats=woff --subset=source/static/fonts/Inter/Inter-300-normal.woff --whitelist=HomeBlogEventsProjects
+glyphhanger --formats=woff,woff2 --subset=source/static/fonts/Inter/Inter-900-normal.woff2 --US_ASCII
+glyphhanger --formats=woff,woff2 --subset=source/static/fonts/Inter/Inter-900-italic.woff2 --US_ASCII
+glyphhanger --formats=woff,woff2 --subset=source/static/fonts/Inter/Inter-700-normal.woff2 --US_ASCII
+glyphhanger --formats=woff,woff2 --subset=source/static/fonts/Inter/Inter-700-italic.woff2 --US_ASCII
+glyphhanger --formats=woff,woff2 --subset=source/static/fonts/Inter/Inter-400-normal.woff2 --US_ASCII
+glyphhanger --formats=woff,woff2 --subset=source/static/fonts/Inter/Inter-400-italic.woff2 --US_ASCII
+glyphhanger --formats=woff,woff2 --subset=source/static/fonts/Inter/Inter-300-normal.woff2 --whitelist=HomeBlogEventsProjects
