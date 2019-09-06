2d1564782e8a2fe04984d8f8cc2b4668a62e9267
Arnelle Balane <arnellebalane@gmail.com>
2019-09-06 22:56:33 +0800

    Remove formatArticleDate and use formatDate instead

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,6 @@ module.exports = config => {
     config.addFilter('filterProjects', require('./source/_filters/filterProjects'));
     config.addFilter('filterLibraries', require('./source/_filters/filterLibraries'));
     config.addFilter('formatDate', require('./source/_filters/formatDate'));
-    config.addFilter('formatArticleDate', require('./source/_filters/formatArticleDate'));
     config.addFilter('formatPageTitle', require('./source/_filters/formatPageTitle'));
     config.addShortcode('externalLink', require('./source/_shortcodes/externalLink'));
 
--- source/_filters/formatArticleDate.js
+++
@@ -1,5 +0,0 @@
-const format = require('date-fns/format');
-
-module.exports = date => {
-    return format(date, 'MMMM d, yyyy');
-};
