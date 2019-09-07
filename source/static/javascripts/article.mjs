3bfbe6dadfa1ed507bddfe1db12d9d4bbbd115df
Arnelle Balane <arnellebalane@gmail.com>
2019-09-07 16:41:25 +0800

    Remove title suffix when sharing article

--- source/static/javascripts/article.mjs
+++ source/static/javascripts/article.mjs
@@ -1,21 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const link = document.querySelector('.js-Share');
     const url = location.origin + location.pathname
+    const title = document.title.replace(/|\s*Arnelle Balane$/, '');
 
     if (navigator.share) {
         link.textContent = 'Share this article';
         link.addEventListener('click', async event => {
             event.preventDefault();
-
-            await navigator.share({
-                title: document.title,
-                text: document.title,
-                url
-            });
+            await navigator.share({text: document.title, title, url});
         });
 
     } else {
-        const title = document.title.replace(/|\s*Arnelle Balane$/, '');
         const text = `"${title}" by @arnellebalane: ${url}`;
         link.textContent = 'Tweet this article';
         link.href = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}`;
